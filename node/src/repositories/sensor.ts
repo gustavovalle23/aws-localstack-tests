04690bf098bcbfe6fe8f1615b88ab2583f5640c8
Gustavo vallerp <gustavo.vallerp@hotmail.com>
2023-07-29 12:07:46 -0300

    :sparkles: feat: add repository in routers

--- node/src/repositories/sensor.ts
+++ node/src/repositories/sensor.ts
@@ -1,115 +1,91 @@
 import AWS from 'aws-sdk';
-import { v4 as uuidv4 } from 'uuid';
-import dotenv from 'dotenv';
-import { createDynamoDBClient } from '../gateways/dynamodbExample';
 import { DeleteSensorInput, SensorData, UpdateSensorInput } from './types';
 
-dotenv.config();
 
-const docClient = createDynamoDBClient()
+export class SensorRepository {
+  private docClient: AWS.DynamoDB.DocumentClient;
+  private sensorDataTable: string;
 
-const sensorDataTable = 'SensorData';
-
-async function createSensorData({ PatientID, SensorID, Value, SensorType, Timestamp }: SensorData): Promise<void> {
-  const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
-    TableName: sensorDataTable,
-    Item: {
-      SensorID,
-      Timestamp,
-      PatientID,
-      SensorType,
-      Value
-    },
-  };
-
-  try {
-    await docClient.put(params).promise();
-    console.log('Sensor Data created successfully.');
-  } catch (error) {
-    console.error('Error creating Sensor Data:', error);
+  constructor(docClient: AWS.DynamoDB.DocumentClient, tableName: string) {
+    this.docClient = docClient;
+    this.sensorDataTable = tableName;
   }
-}
 
-async function getSensorData(sensorID: string, timestamp: string): Promise<void> {
-  const params: AWS.DynamoDB.DocumentClient.GetItemInput = {
-    TableName: sensorDataTable,
-    Key: {
-      SensorID: sensorID,
-      Timestamp: timestamp,
-    },
-  };
+  async createSensorData({ PatientID, SensorID, Value, SensorType, Timestamp }: SensorData): Promise<AWS.DynamoDB.DocumentClient.PutItemOutput | void> {
+    const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+      TableName: this.sensorDataTable,
+      Item: {
+        SensorID,
+        Timestamp,
+        PatientID,
+        SensorType,
+        Value
+      },
+    };
 
-  try {
-    const result = await docClient.get(params).promise();
-    if (result.Item) {
-      console.log('Sensor Data:', result.Item);
-    } else {
-      console.log('Sensor Data not found.');
+    try {
+      return (await this.docClient.put(params).promise()).$response.data;
+    } catch (error) {
+      console.error('Error creating Sensor Data:', error);
     }
-  } catch (error) {
-    console.error('Error retrieving Sensor Data:', error);
+  }
+
+  async getSensorData(sensorID: string, timestamp: string): Promise<AWS.DynamoDB.DocumentClient.AttributeMap | void> {
+    const params: AWS.DynamoDB.DocumentClient.GetItemInput = {
+      TableName: this.sensorDataTable,
+      Key: {
+        SensorID: sensorID,
+        Timestamp: timestamp,
+      },
+    };
+
+    try {
+      const result = await this.docClient.get(params).promise();
+      if (result.Item) {
+        return result.Item
+      } else {
+        console.log('Sensor Data not found.');
+      }
+    } catch (error) {
+      console.error('Error retrieving Sensor Data:', error);
+    }
+  }
+
+  async updateSensorData({ SensorID, Timestamp, SensorType, Value }: UpdateSensorInput): Promise<void> {
+    const params: AWS.DynamoDB.DocumentClient.UpdateItemInput = {
+      TableName: this.sensorDataTable,
+      Key: {
+        SensorID,
+        Timestamp,
+      },
+      UpdateExpression: 'set SensorType = :st, Value = :val',
+      ExpressionAttributeValues: {
+        ':st': SensorType,
+        ':val': Value,
+      },
+      ReturnValues: 'UPDATED_NEW',
+    };
+
+    try {
+      await this.docClient.update(params).promise();
+    } catch (error) {
+      console.error('Error updating Sensor Data:', error);
+    }
+  }
+
+  async deleteSensorData({ SensorID, Timestamp }: DeleteSensorInput): Promise<void> {
+    const params: AWS.DynamoDB.DocumentClient.DeleteItemInput = {
+      TableName: this.sensorDataTable,
+      Key: {
+        SensorID,
+        Timestamp,
+      },
+    };
+
+    try {
+      await this.docClient.delete(params).promise();
+    } catch (error) {
+      console.error('Error deleting Sensor Data:', error);
+    }
   }
 }
-
-async function updateSensorData({ SensorID, Timestamp, SensorType, Value }: UpdateSensorInput): Promise<void> {
-  const params: AWS.DynamoDB.DocumentClient.UpdateItemInput = {
-    TableName: sensorDataTable,
-    Key: {
-      SensorID,
-      Timestamp,
-    },
-    UpdateExpression: 'set SensorType = :st, Value = :val',
-    ExpressionAttributeValues: {
-      ':st': SensorType,
-      ':val': Value,
-    },
-    ReturnValues: 'UPDATED_NEW',
-  };
-
-  try {
-    await docClient.update(params).promise();
-    console.log('Sensor Data updated successfully.');
-  } catch (error) {
-    console.error('Error updating Sensor Data:', error);
-  }
-}
-
-async function deleteSensorData({ SensorID, Timestamp }: DeleteSensorInput): Promise<void> {
-  const params: AWS.DynamoDB.DocumentClient.DeleteItemInput = {
-    TableName: sensorDataTable,
-    Key: {
-      SensorID,
-      Timestamp,
-    },
-  };
-
-  try {
-    await docClient.delete(params).promise();
-    console.log('Sensor Data deleted successfully.');
-  } catch (error) {
-    console.error('Error deleting Sensor Data:', error);
-  }
-}
-
-async function main(): Promise<void> {
-  const SensorID = uuidv4();
-  const Timestamp = new Date().toISOString();
-
-  await createSensorData({ PatientID: 'patient-1', SensorID, SensorType: 'presence', Timestamp, Value: 1 });
-
-  await getSensorData('sensor-1', '2023-07-23T10:00:00Z');
-
-  await updateSensorData({
-    SensorID: 'sensor-1',
-    Timestamp: '2023-07-23T10:00:00Z',
-    SensorType: 'heart-rate',
-    Value: 80
-  });
-
-  await deleteSensorData({
-    SensorID: 'sensor-1',
-    Timestamp: '2023-07-23T10:00:00Z'
-  });
-}
-
-main();
--- node/src/routers/sensor.ts
+++ node/src/routers/sensor.ts
@@ -1,22 +1,31 @@
 import express from 'express'
+import { SensorRepository } from '../repositories'
+import { createDynamoDBClient } from '../gateways/dynamodbExample'
 
 export const sensorRouter = express.Router()
+
+const docClient = createDynamoDBClient()
+const repository = new SensorRepository(docClient, 'SensorData')
 
 sensorRouter.get('/', (req, res) => {
   res.send({ message: 'Get all sensor data' })
 })
 
-sensorRouter.get('/:id', (req, res) => {
+sensorRouter.get('/:id', async (req, res) => {
   const sensorId = req.params.id
-  res.send({ message: `Get sensor with ID ${sensorId}` })
+  const sensor = await repository.getSensorData(sensorId, '')
+  res.send({ message: sensor })
 })
 
-sensorRouter.put('/:id', (req, res) => {
-  const sensorId = req.params.id
-  res.send({ message: `Updated sensor with ID ${sensorId} to: ${JSON.stringify(req.body)}` });
+sensorRouter.put('/:id', async (req, res) => {
+  const SensorID = req.params.id
+  const { SensorType, Timestamp, Value } = req.body
+  await repository.updateSensorData({ SensorID, SensorType, Timestamp, Value })
+  res.send({ message: `Updated sensor with ID ${SensorID} to: ${JSON.stringify(req.body)}` });
 })
 
-sensorRouter.delete('/:id', (req, res) => {
-  const sensorId = req.params.id;
-  res.send({ message: `Delete sensor with ID ${sensorId}` });
+sensorRouter.delete('/:id', async (req, res) => {
+  const SensorID = req.params.id;
+  await repository.deleteSensorData({ SensorID, Timestamp: '' })
+  res.send({ message: `Delete sensor with ID ${SensorID}` });
 });
